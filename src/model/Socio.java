bf3263771be1b6300bac54acfa7a7baabf6d816e
gustavo-gomess <gusgegomes@hotmail.com>
2023-05-15 21:32:48 -0300

    Criando a base das pastas para iniciar o projeto

---
+++ src/model/Socio.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Socio {
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Socio(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Socio{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
