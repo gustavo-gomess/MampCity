71504fa9f1d7c73d896105eb0bad547fd4e87aa1
luc4sdemetrio <lucasdemetriodeabreu@gmail.com>
2023-05-17 20:27:50 -0300

    Criado classe pessoa

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,49 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("esse é o nosso projeto");
+public class Pessoa {
+
+    String nomeCompleto
+    String cpf
+    Integer telefone
+    String email
+    String status
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Integer getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Integer telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
 
