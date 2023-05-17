ab223a9a7576a36989977ef53b6b1fcc18b9ce66
luc4sdemetrio <lucasdemetriodeabreu@gmail.com>
2023-05-17 20:39:02 -0300

    Criado classe pessoa dinovo

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,6 @@
-public class Pessoa {
 
-    String nomeCompleto
-    String cpf
-    Integer telefone
-    String email
-    String status
+public class Main {
+    public static void main(String[] args) {
 
-    public String getNomeCompleto() {
-        return nomeCompleto;
-    }
-
-    public void setNomeCompleto(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Integer getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(Integer telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 }
-
